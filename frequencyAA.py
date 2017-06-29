0b2f566829a4602bd048ff05c2f2164d85dbef31
Kazutoshi Takahashi <kazutoshi@KazuUbuntu.(none)>
2017-06-29 15:19:07 -0700

    updated loop data

---
+++ frequencyAA.py
@@ -0,0 +1,56 @@
+import sys
+stripping = []
+elements =[]
+record = []
+counting = 12
+placeholding = 0
+
+
+while True:
+	with open(sys.argv[1]) as f:	
+		for line in f:			
+			stripping+= line.strip()
+			elements += stripping[-12+counting+placeholding]
+			counting+= 12
+			print elements
+			if counting >131:
+				placeholding+=1
+				record.append(elements)
+				print record
+				stripping = []
+				elements =[]
+				record = []
+				counting = 12
+				dataset_list = ''.join(stripping)
+				print dataset_list
+
+	if placeholding > 11:
+		break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#sys.exit()
+#dataset_list = ''.join(stripping)
+#print dataset_list
+#print firstElements
---
+++ testingground.py
@@ -0,0 +1,125 @@
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+2
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
+
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+3
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+4
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
+
+
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+5
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
+
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+6
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
+
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+7
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
+
+
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+8
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+9
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
+
+with open(sys.argv[1]) as f:	
+	for line in f:			
+		stripping+= line.strip()
+		elements += stripping[-12+counting+placeholding]
+		counting+= 12
+		if counting >131:
+			placeholding=+10
+			record.append(elements)
+			print record
+			stripping = []
+			elements =[]
+			record = []
+			counting = 12
